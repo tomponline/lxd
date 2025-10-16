a400a16a6c918d25c82e659c5aa7d7d7bb5fca25
Thomas Parrott <thomas.parrott@canonical.com>
2025-10-16 14:07:44 +0100

    lxd/instance/drivers/driver/qemu: Improve errors in deviceAttachNIC and deviceAttachBlockDevice
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/instance/drivers/driver_qemu.go
+++ lxd/instance/drivers/driver_qemu.go
@@ -2464,7 +2464,7 @@ func (d *qemu) deviceAttachBlockDevice(mount deviceConfig.MountEntryItem) error 
 
 	err = monHook(monitor)
 	if err != nil {
-		return fmt.Errorf("Failed to call monitor hook for block device: %w", err)
+		return fmt.Errorf("Failed setting up device via monitor: %w", err)
 	}
 
 	return nil
@@ -2581,7 +2581,7 @@ func (d *qemu) deviceAttachNIC(netIF []deviceConfig.RunConfigItem) error {
 
 	err = monHook(monitor)
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed setting up device via monitor: %w", err)
 	}
 
 	return nil
