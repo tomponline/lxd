57c02da75744d09bb69a4aca2e444d3715fd66d4
Thomas Parrott <thomas.parrott@canonical.com>
2026-07-06 15:54:25 +0100

    lxd/device: Dont setup tap multi-queue for libkrun
    Signed-off-by: Thomas Parrott <thomas.parrott@canonical.com>

--- lxd/device/nic_bridged.go
+++ lxd/device/nic_bridged.go
@@ -524,7 +524,7 @@ func (d *nicBridged) Start() (*deviceConfig.RunConfig, error) {
 			}
 		}
 		peerName = saveData["host_name"] // VMs use the host_name to link to the TAP FD.
-		mtu, err = networkCreateTap(saveData["host_name"], d.config)
+		mtu, err = networkCreateTap(saveData["host_name"], d.config, networkTapMultiQueue(d.inst))
 	}
 
 	if err != nil {
